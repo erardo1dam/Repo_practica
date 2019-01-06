5b715c8f1440664d6691a9849a7c3441aaa62ebe
user <user@DESKTOP-MT31T1Q>
2019-01-06 21:31:38 +0100

    Commit de al1
    commit al1

--- PracticaGit/Practica/Al1.java
+++ PracticaGit/Practica/Al1.java
@@ -4,12 +4,9 @@ public class Al1 {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("Hola soy alumno 1");
-<<<<<<< HEAD
 		System.out.println("Hola soy alumno 2");
+		System.out.println("Cambios alumno 1");
 		
-=======
->>>>>>> refs/remotes/origin/master
-
 	}
 
 }
