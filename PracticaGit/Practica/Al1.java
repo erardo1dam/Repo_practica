8b966e9dbb519938f6217523b8257759af155878
alu2 <user@DESKTOP-MT31T1Q>
2019-01-06 21:40:30 +0100

    Commit de rama nueva alu2

--- PracticaGit/Practica/Al1.java
+++ PracticaGit/Practica/Al1.java
@@ -4,11 +4,9 @@ public class Al1 {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("Hola soy alumno 1");
-<<<<<<< HEAD
 		System.out.println("Hola soy alumno 2");
+		System.out.println("Esto es una rama nueva");
 		
-=======
->>>>>>> refs/remotes/origin/master
 
 	}
 
